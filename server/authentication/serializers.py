9b29b86702f1fa7630718eb5df5e19241ee54a49
tahere fahimi <t.fahimi1998@gmail.com>
2021-01-16 11:00:38 +0330

    authenticatioproblems removed

--- server/authentication/serializers.py
+++ server/authentication/serializers.py
@@ -26,3 +26,46 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+    
+    def create(self, validated_data):
+        with transaction.atomic():
+            type = validated_data.pop("type")
+            if 'doctor' in validated_data:
+                doctor = validated_data.pop("doctor")
+            instance: User = super(UserSerializer, self).create(validated_data)
+            if type == "doctor":
+                group = Group.objects.get(name="doctor")
+                data = doctor
+                data.update({"user": instance.id, "spec": data["spec"].id})
+                serializer = DoctorHelpSerializer(data=data)
+                if serializer.is_valid(raise_exception=True):
+                    serializer.save()
+            else:
+                group = Group.objects.get(name="client")
+            instance.groups.add(group)
+            instance.set_password(self.initial_data["password"])
+            instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        with transaction.atomic():
+            if 'doctor' in validated_data:
+                doctor = validated_data.pop("doctor")
+            if 'password' in validated_data:
+                password = validated_data.pop("password")
+            if 'type' in validated_data:
+                type = validated_data.pop('type')
+            if 'username' in validated_data:
+                validated_data.pop('username')
+            instance: User = super(UserSerializer, self).update(instance, validated_data)
+            if instance.type == "doctor":
+                data = doctor
+                data.update({"spec": data["spec"].id})
+                serializer = DoctorHelpSerializer(instance=instance.doctor, data=data)
+                if serializer.is_valid(raise_exception=True):
+                    serializer.save()
+
+            if password:
+                instance.set_password(self.initial_data["password"])
+                instance.save()
+        return instance
